080628031afba181e8863f939fdafb13073b7ef7
Tomasz11111 <tomasz.dziendziura@gmail.com>
2017-12-06 18:55:21 +0100

    zadanie 1.3

--- program.js
+++ program.js
@@ -3,21 +3,23 @@ process.stdin.on('readable', function() {
 	var input = process.stdin.read();
 	if(input !== null) {
 		var instruction = input.toString().trim();
-		if (instruction === '/exit') {
-			process.stdout.write('Quiting app!\n');
-			process.exit();
-		}
-		else if(instruction === 'version') {
-			var version = process.versions.node;
-			process.stdout.write(version);
-		}
-		else if(instruction === 'lang') /*only for Mac and Linux*/ {
-			var lang = process.env.LANG;
-			process.stdout.write(lang);
+		switch(instruction) {
+			case 'version':
+				var version = process.versions.node;
+				process.stdout.write(version);
+				break;
+			case 'lang':
+				var lang = process.env.LANG; /*only for Mac and Linux*/
+				process.stdout.write(lang);
+				break;
+			case '/exit':
+				process.stdout.write('Quiting app!\n');
+				process.exit();
+				break;
+			default:
+				process.stderr.write("Wrong instruction!\n");
+			
 			}
-		else 
-		{
-			process.stderr.write("Wrong instruction!\n");
-		}
+		
 	}
 });
